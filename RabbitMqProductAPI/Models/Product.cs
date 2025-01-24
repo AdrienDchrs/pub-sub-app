e5883202c1644f99b7523118199d3da2c930c9ff
Adrien Decharrois <95859046+AdrienDchrs@users.noreply.github.com>
2025-01-24 13:55:10 +0100

    correction issues

--- RabbitMqProductAPI/Models/Product.cs
+++ RabbitMqProductAPI/Models/Product.cs
@@ -2,10 +2,19 @@
 {
     public class Product
     {
+		[JsonRequired]
         public int ProductId { get; set; }
+
+		[JsonRequired]
         public string ProductName { get; set; }
+
+		[JsonRequired]
         public string ProductDescription { get; set; }
+
+		[JsonRequired]
         public int ProductPrice { get; set; }
+		
+		[JsonRequired]
         public int ProductStock { get; set; }
     }
 }
