f5752ea2ad49b1f2f3513136b5adb56725a8f4a4
Adrien Decharrois <95859046+AdrienDchrs@users.noreply.github.com>
2025-01-24 14:06:13 +0100

    upd

--- RabbitMqProductAPI/Models/Product.cs
+++ RabbitMqProductAPI/Models/Product.cs
@@ -2,19 +2,10 @@
 {
     public class Product
     {
-		[JsonRequired]
         public int ProductId { get; set; }
-
-		[JsonRequired]
         public string ProductName { get; set; }
-
-		[JsonRequired]
         public string ProductDescription { get; set; }
-
-		[JsonRequired]
         public int ProductPrice { get; set; }
-		
-		[JsonRequired]
         public int ProductStock { get; set; }
     }
 }
